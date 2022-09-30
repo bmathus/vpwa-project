993900a66b0762d3abca0cec075aa2c6cf8af89a
bmathus <xbojko@stuba.sk>
2022-09-30 18:04:28 +0200

    user avatar in drawer

--- app-frontend/src/layouts/MainLayout.vue
+++ app-frontend/src/layouts/MainLayout.vue
@@ -5,26 +5,34 @@
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="drawer = !drawer" />
         <q-toolbar-title>
-          <div class="text-weight-bolder">Channel 1</div>
+          <div class="text-h6">Channel 1</div>
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="drawer" side="left" :width="200" :breakpoint="500" bordered class="bg-grey-3">
-      <q-scroll-area class="fit">
-        <q-list dense>
+    <q-drawer show-if-above v-model="drawer" side="left" :width="200" :breakpoint="500" class="bg-grey-3">
 
+      <div class="absolute-top row items-center" style="height:50px;">
+        <q-btn unelevated color="primary" padding="none" style="height:32px; width:32px" label="M" class="q-ma-sm"
+          text-color="black">
+          <q-badge color="green" class="absolute-bottom-right badge-position" rounded />
+        </q-btn>
+        <div class="text-weight-bold">User Name</div>
+        <q-separator class="absolute-bottom"></q-separator>
+      </div>
+
+      <q-scroll-area style="height: calc(100% - 50px); margin-top: 50px;">
+        <q-list dense>
           <template v-for="(menuItem, index) in menuList" :key="index">
             <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
               <q-item-section>
                 # {{ menuItem.label }}
               </q-item-section>
             </q-item>
-            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
           </template>
-
         </q-list>
       </q-scroll-area>
+
     </q-drawer>
 
     <q-page-container>
@@ -46,32 +54,50 @@ import { ref, defineComponent } from 'vue'
 const menuList = [
   {
     label: 'Channel 1',
-    separator: true
   },
   {
     label: 'Channel 2',
-    separator: false
   },
   {
     label: 'Channel 3',
-    separator: false
   },
   {
     label: 'Channel 4',
-    separator: true
   },
   {
     label: 'Channel 5',
-    separator: false
   },
   {
     label: 'Channel 6',
-    separator: false
   },
   {
     label: 'Channel 7',
-    separator: false
-  }
+  },
+  {
+    label: 'Channel 5',
+  },
+  {
+    label: 'Channel 6',
+  },
+  {
+    label: 'Channel 7',
+  },
+  {
+    label: 'Channel 5',
+  },
+  {
+    label: 'Channel 6',
+  },
+  {
+    label: 'Channel 7',
+  },
+  {
+    label: 'Channel 5',
+  },
+  {
+    label: 'Channel 6',
+  },
+
 ]
 
 export default defineComponent({
@@ -86,3 +112,9 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+.badge-position {
+  margin: 0px -2px -2px 0px
+}
+</style>
