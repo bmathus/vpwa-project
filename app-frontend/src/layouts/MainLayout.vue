c11481aa055a050b9b0b98b39e8359ab600db1c0
bmathus <xbojko@stuba.sk>
2022-09-30 14:42:22 +0200

    main app layeout and font

--- app-frontend/src/layouts/MainLayout.vue
+++ app-frontend/src/layouts/MainLayout.vue
@@ -1,14 +1,48 @@
 <template>
-  <div>
-    hello world
-  </div>
+  <q-layout view="lHh Lpr lFf">
+
+    <q-header class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-toolbar-title>
+          <div>Channel name</div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer class="bg-grey-8 text-white">
+      <div class=" bg-amber">
+        textbox will be here
+      </div>
+    </q-footer>
+
+  </q-layout>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { ref, defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'MainLayout'
-});
+  name: 'MainLayout',
+  setup() {
+    const leftDrawerOpen = ref(false)
 
+    function toggleLeftDrawer() {
+      leftDrawerOpen.value = !leftDrawerOpen.value
+    }
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer
+    }
+  }
+});
 </script>
