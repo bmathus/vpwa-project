b12c52309a5791c4ae56bf98f8a5ee7ede6964be
bmathus <xbojko@stuba.sk>
2022-09-30 15:30:50 +0200

    header color and drawer channels

--- app-frontend/src/layouts/MainLayout.vue
+++ app-frontend/src/layouts/MainLayout.vue
@@ -1,17 +1,30 @@
 <template>
   <q-layout view="lHh Lpr lFf">
 
-    <q-header class="bg-primary text-white">
+    <q-header class="bg-dark text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-btn dense flat round icon="menu" @click="drawer = !drawer" />
         <q-toolbar-title>
-          <div>Channel name</div>
+          <div class="text-weight-bolder">Channel 1</div>
         </q-toolbar-title>
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left">
-      <!-- drawer content -->
+    <q-drawer show-if-above v-model="drawer" side="left" :width="200" :breakpoint="500" bordered class="bg-grey-3">
+      <q-scroll-area class="fit">
+        <q-list dense>
+
+          <template v-for="(menuItem, index) in menuList" :key="index">
+            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
+              <q-item-section>
+                # {{ menuItem.label }}
+              </q-item-section>
+            </q-item>
+            <q-separator :key="'sep' + index" v-if="menuItem.separator" />
+          </template>
+
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
     <q-page-container>
@@ -30,18 +43,45 @@
 <script lang="ts">
 import { ref, defineComponent } from 'vue'
 
+const menuList = [
+  {
+    label: 'Channel 1',
+    separator: true
+  },
+  {
+    label: 'Channel 2',
+    separator: false
+  },
+  {
+    label: 'Channel 3',
+    separator: false
+  },
+  {
+    label: 'Channel 4',
+    separator: true
+  },
+  {
+    label: 'Channel 5',
+    separator: false
+  },
+  {
+    label: 'Channel 6',
+    separator: false
+  },
+  {
+    label: 'Channel 7',
+    separator: false
+  }
+]
+
 export default defineComponent({
   name: 'MainLayout',
   setup() {
-    const leftDrawerOpen = ref(false)
-
-    function toggleLeftDrawer() {
-      leftDrawerOpen.value = !leftDrawerOpen.value
-    }
+    const drawer = ref(false)
 
     return {
-      leftDrawerOpen,
-      toggleLeftDrawer
+      drawer,
+      menuList
     }
   }
 });
